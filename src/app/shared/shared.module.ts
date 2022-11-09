0fd8bccf743ee2cfe7daeb67af050e295fd2f5db
smooooky <75350537+smooooky@users.noreply.github.com>
2022-11-09 13:28:42 -0500

    added shared module and card and table components to shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { TableComponent } from './components/table/table.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TableComponent,
+    CardComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    TableComponent,
+    CardComponent
   ]
 })
 export class SharedModule { }
